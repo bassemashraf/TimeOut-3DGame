d48873e28f46b806f1832953ac0ed1facfd0a15d
beshoyamgadd <62971744+beshoyamgadd@users.noreply.github.com>
2020-04-01 22:07:05 +0200

    .
    .

---
+++ time out/Assets/scripts/MyDataa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MyDataa
+{
+    public MyDataa()
+    {
+    }
+    public void save(int s)
+    {
+        PlayerPrefs.SetInt("HS", s);
+    }
+    public int load()
+    {
+        int x;
+        x = PlayerPrefs.GetInt("HS");
+        return x;
+    }
+
+}
--- time out/Assets/scripts/playermang.cs
+++ time out/Assets/scripts/playermang.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System;
 public class playermang : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -12,36 +12,94 @@ public class playermang : MonoBehaviour
     public GameObject startingtext;
     public static float Timeseconds;
     public Text time;
+    public Text Gamescore;
+    public Text Highscore;
+    public float scoree = 0;
+    float x = 1 / 100;
+
+    MyDataa m = new MyDataa();
     void Start()
     {
+
         GameOver = false;
         Time.timeScale = 1;
         gamestarted = false;
         Timeseconds = 30f;
+
+        //   Highscore.text = "anaanas";
+        //m.save(5);
+        //  PlayerPrefs.SetInt("HS", 2);
+
+        Highscore.text = "" + m.load();
+        //Highscore.text= (PlayerPrefs.GetInt("HS")).ToString();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameOver|| time.text == "" + 0)
+
+        Gamescore.text = "" + (int)scoree;
+        //   Gamescore.text = "BASD";
+
+
+        if (GameOver || time.text == "" + 0)
         {
             Time.timeScale = 0;
+
+
             gameoverpanel.SetActive(true);
         }
-        if (gamestarted&&Timeseconds>0)
+        if (GameOver)
         {
-                Timeseconds -= Time.deltaTime * 2;
+            //  if (scoree > PlayerPrefs.GetInt("HS"))
+            //  {
+            //      PlayerPrefs.SetInt("HS", scoree);
+            //   }
+            //  Highscore.text = PlayerPrefs.GetInt("HS").ToString();
+            //  if (scoree > mydata.load());
+            //  {
+            //    mydata.save(scoree);
+            //   }
+
+            if ((int)scoree > m.load())
+            {
+                m.save((int)scoree);
+            }
+            //Gamescore.text =scoree.ToString();
+
+
+            // Highscore.text = (PlayerPrefs.GetInt("HS")).ToString();
+            //Highscore.text = (mydata.load()).tostring;
+            Highscore.text = "" + m.load();
+
+
+
         }
-        time.text = "" + (int)Timeseconds; 
+        if (gamestarted && Timeseconds > 0)
+        {
+            Timeseconds -= Time.deltaTime * 2;
+
+        }
+        if (!gamestarted)
+        {
+            scoree = 0;
+        }
+        if (gamestarted && !GameOver)
+        {
+
+            // scoree = scoree + 1;
+            scoree = scoree + Time.deltaTime * 10;
+        }
+        time.text = "" + (int)Timeseconds;
         if (swipemanager.tap)
         {
-            
+
             gamestarted = true;
             Destroy(startingtext);
         }
-        //asdasdasssa
+
     }
-  
-   
+
+
 }
