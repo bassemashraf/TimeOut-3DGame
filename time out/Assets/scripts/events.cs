12c08f53a2507396e9896938f50a1e93f4d45a5a
bassemashraf <52628535+bassemashraf@users.noreply.github.com>
2020-04-02 04:21:49 +0200

    Update events.cs

--- time out/Assets/scripts/events.cs
+++ time out/Assets/scripts/events.cs
@@ -31,7 +31,7 @@ public class events : MonoBehaviour
     {
         PlayerPrefs.SetString("R", "true");
         playermang.revive = true;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.0f);
         playermang.revive = false;
     }
 
